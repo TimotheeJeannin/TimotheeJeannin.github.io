5ec24178d035945279c4df0a49741058c7258213
Timothée Jeannin <timojeajea@gmail.com>
2015-03-15 00:55:46 +0100

    Better skill organisation for ordering and stuff.

--- index.js
+++ index.js
@@ -1,83 +1,119 @@
 angular.module('App', [])
     .controller('Controller', function ($scope) {
-        $scope.skills = {
-            'Front-End': {
-                'FontAwesome': '100%',
-                'Less': '70%',
-                'AngularJs': '90%',
-                'React': '10%',
-                'jQuery': '70%',
-                'Bootstrap': '100%',
-                'Bower': '90%',
-                'NPM': '80%',
-                'Grunt': '70%',
-                'Karma': '80%',
-                'D3.js': '60%',
-                'Jasmine': '90%'
+        $scope.skillSets = [
+            {
+                'name': 'Front-End',
+                'skills': [
+                    {'name': 'FontAwesome', 'evaluation': '99%'},
+                    {'name': 'Less', 'evaluation': '70%'},
+                    {'name': 'AngularJs', 'evaluation': '90%'},
+                    {'name': 'React', 'evaluation': '10%'},
+                    {'name': 'jQuery', 'evaluation': '70%'},
+                    {'name': 'Bootstrap', 'evaluation': '99%'},
+                    {'name': 'Bower', 'evaluation': '90%'},
+                    {'name': 'NPM', 'evaluation': '80%'},
+                    {'name': 'Grunt', 'evaluation': '70%'},
+                    {'name': 'Karma', 'evaluation': '80%'},
+                    {'name': 'D3.js', 'evaluation': '60%'},
+                    {'name': 'Jasmine', 'evaluation': '90%'}
+                ]
             },
-            'Back-End': {
-                'Django': '40%',
-                'Flask': '90%',
-                'Spring': '15%',
-                'Express': '20%'
+            {
+                'name': 'Back-End',
+                'skills': [
+                    {'name': 'Django', 'evaluation': '40%'},
+                    {'name': 'Flask', 'evaluation': '90%'},
+                    {'name': 'Spring', 'evaluation': '15%'},
+                    {'name': 'Express', 'evaluation': '20%'}
+                ]
             },
-            'Databases': {
-                'Redis': '60%',
-                'MongoDB': '20%',
-                'SQLite': '90%',
-                'MySQL': '80%'
+            {
+                'name': 'Databases',
+                'skills': [
+                    {'name': 'Redis', 'evaluation': '60%'},
+                    {'name': 'MongoDB', 'evaluation': '20%'},
+                    {'name': 'SQLite', 'evaluation': '90%'},
+                    {'name': 'MySQL', 'evaluation': '80%'}
+                ]
             },
-            'Mobile': {
-                'Android SDK': '80%'
+            {
+                'name': 'Mobile',
+                'skills': [
+                    {'name': 'Android SDK', 'evaluation': '80%'}
+                ]
             },
-            'Games': {
-                'libGDX': '20%',
-                'Unity3D': '10%'
+            {
+                'name': 'Games',
+                'skills': [
+                    {'name': 'libGDX', 'evaluation': '20%'},
+                    {'name': 'Unity3D', 'evaluation': '10%'}
+                ]
             },
-            'Version Control': {
-                'Git': '100%',
-                'Subversion': '90%'
+            {
+                'name': 'Version Control',
+                'skills': [
+                    {'name': 'Git', 'evaluation': '100%'},
+                    {'name': 'Subversion', 'evaluation': '90%'}
+                ]
             },
-            'Continuous Integration': {
-                'Jenkins': '80%',
-                'Travis': '60%',
-                'TeamCity': '20%'
+            {
+                'name': 'Continuous Integration',
+                'skills': [
+                    {'name': 'Jenkins', 'evaluation': '80%'},
+                    {'name': 'Travis', 'evaluation': '60%'},
+                    {'name': 'TeamCity', 'evaluation': '20%'}
+                ]
             },
-            'Languages': {
-                'Html': '90%',
-                'Css': '90%',
-                'Bash': '40%',
-                'JavaScript': '90%',
-                'Python': '90%',
-                'C': '30%',
-                'C++': '40%',
-                'C#': '20%',
-                'Java': '90%',
-                'SQL': '80%'
+            {
+                'name': 'Languages',
+                'skills': [
+                    {'name': 'Html', 'evaluation': '90%'},
+                    {'name': 'Css', 'evaluation': '90%'},
+                    {'name': 'Bash', 'evaluation': '40%'},
+                    {'name': 'JavaScript', 'evaluation': '90%'},
+                    {'name': 'Python', 'evaluation': '90%'},
+                    {'name': 'C', 'evaluation': '30%'},
+                    {'name': 'C++', 'evaluation': '40%'},
+                    {'name': 'C#', 'evaluation': '20%'},
+                    {'name': 'Java', 'evaluation': '90%'},
+                    {'name': 'SQL', 'evaluation': '80%'}
+                ]
             },
-            'Operating Systems': {
-                'Ubuntu': '90%',
-                'Windows': '80%',
-                'OS X': '30%'
+            {
+                'name': 'Operating Systems',
+                'skills': [
+                    {'name': 'Ubuntu', 'evaluation': '90%'},
+                    {'name': 'Windows', 'evaluation': '80%'},
+                    {'name': 'OS X', 'evaluation': '30%'}
+                ]
             },
-            'IDE': {
-                'Intellij IDEA': '90%',
-                'Eclipse': '60%'
+            {
+                'name': 'IDE',
+                'skills': [
+                    {'name': 'Intellij IDEA', 'evaluation': '90%'},
+                    {'name': 'Eclipse', 'evaluation': '60%'}
+                ]
             },
-            'Deployment': {
-                'Fabric': '70%',
-                'Gunicorn': '70%',
-                'Docker': '30%',
-                'Tomcat': '20%',
-                'Puppet': '30%'
+            {
+                'name': 'Deployment',
+                'skills': [
+                    {'name': 'Fabric', 'evaluation': '70%'},
+                    {'name': 'Gunicorn', 'evaluation': '70%'},
+                    {'name': 'Docker', 'evaluation': '30%'},
+                    {'name': 'Tomcat', 'evaluation': '20%'},
+                    {'name': 'Puppet', 'evaluation': '30%'}
+                ]
             },
-            'Cloud': {
-                'Amazon S3': '80%',
-                'Amazon EC2': '60%',
-                'Amazon Route 53': '40%',
-                'YouTube API': '60%',
-                'Google Admin SDK': '60%',
-                'Google Cloud Messaging': '40%'
+            {
+                'name': 'Cloud',
+                'skills': [
+                    {'name': 'Amazon S3', 'evaluation': '80%'},
+                    {'name': 'Amazon EC2', 'evaluation': '60%'},
+                    {'name': 'Amazon Route 53', 'evaluation': '40%'},
+                    {'name': 'YouTube API', 'evaluation': '60%'},
+                    {'name': 'Google Admin SDK', 'evaluation': '60%'},
+                    {'name': 'Google Cloud Messaging', 'evaluation': '40%'}
+                ]
             }
-        };
+        ];
     });
